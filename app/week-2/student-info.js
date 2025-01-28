f5b870ce302e53442b352947acd1da7e674a5270
johram123 <johramq@gmail.com>
2025-01-28 13:29:45 -0700

    Comitting week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export function StudentInfo(){
+    return (
+        <div>
+            <p>Johram Querijero</p>
+            <Link href="https://github.com/johram123/cprg306-assignments">https://github.com</Link>
+        </div>
+    );
+}
