1487a7f61299545af75eb90f5796374361acc106
johram123 <johramq@gmail.com>
2025-03-11 18:59:04 -0600

    Added week-5

---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import { NewItem } from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="flex w-screen h-screen justify-center items-center">
+      <NewItem />
+    </main>
+  );
+}
